25662afffae47f249a2ea356654c377d9db39e34
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2019-06-25 20:15:30 -0700

    Initial commit

---
+++ UnWADIT.py
@@ -0,0 +1,32 @@
+import os
+import struct
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir")
+parser.add_argument("wad")
+args = parser.parse_args()
+
+f0 = open(args.dir,"rb")
+f1 = open(args.wad,"rb")
+
+amount = struct.unpack('i', f0.read(4))[0]
+
+print "Unpacking WAD..."
+
+for i in range(0,amount):
+    name = f0.read(0x40)
+    newname = name.replace('\0', '')
+    print newname
+    size = struct.unpack('i', f0.read(4))[0]
+    address = struct.unpack('i', f0.read(4))[0]
+    f1.seek(address, os.SEEK_SET)
+    filebytes = f1.read(size)
+    f2 = open(newname, "wb")
+    f2.write(filebytes)
+    f2.close()
+
+
+f0.close()
+f1.close()
