48ed9758f6d49576f83edeba0322f4f899f29ad2
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2021-08-01 14:47:50 -0400

    Add main()

--- Repack.py
+++ Repack.py
@@ -2,56 +2,59 @@ import os
 import struct
 import argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("directory")
-args = parser.parse_args()
+    address = 0x00
+    needPadCheck = False
 
-address = 0x00
-needPadCheck = False
+    print("Packing WAD...")
+    fileList = os.listdir(args.directory)
+    f0 = open(args.directory + ".DIR", "wb")
+    f1 = open(args.directory + ".WAD", "wb")
+    numOfFiles = len(fileList)
+    f0.write(struct.pack('I', numOfFiles))
 
-print("Packing WAD...")
-fileList = os.listdir(args.directory)
-f0 = open(args.directory + ".DIR", "wb")
-f1 = open(args.directory + ".WAD", "wb")
-numOfFiles = len(fileList)
-f0.write(struct.pack('I', numOfFiles))
+    for fileName in fileList:
+        f0.write(fileName.encode())
+        padLength = 0x40 - len(fileName)
+        f0.write(bytearray([0x00]) * padLength)
+        filePath = os.path.join(args.directory, fileName)
+        f2 = open(filePath, "rb")
+        header = f2.read(4)
 
-for fileName in fileList:
-    f0.write(fileName.encode())
-    padLength = 0x40 - len(fileName)
-    f0.write(bytearray([0x00]) * padLength)
-    filePath = os.path.join(args.directory, fileName)
-    f2 = open(filePath, "rb")
-    header = f2.read(4)
+        # pad everything that isn't a strat WAD
+        if header != "BIGB".encode():
+            needPadCheck = True
+        else:
+            print(fileName)
 
-    # pad everything that isn't a strat WAD
-    if header != "BIGB".encode():
-        needPadCheck = True
-    else:
-        print(fileName)
+        f2.seek(0x00, os.SEEK_END)
+        size = f2.tell()
+        f2.seek(0x00, os.SEEK_SET)
 
-    f2.seek(0x00, os.SEEK_END)
-    size = f2.tell()
-    f2.seek(0x00, os.SEEK_SET)
+        f0.write(struct.pack('I', size))
+        f0.write(struct.pack('I', address))
 
-    f0.write(struct.pack('I', size))
-    f0.write(struct.pack('I', address))
+        if needPadCheck == True:
+            print("Padding " + fileName)
+            paddingSize = 0x800 - (size % 0x800)
+            address += paddingSize
 
-    if needPadCheck == True:
-        print("Padding " + fileName)
-        paddingSize = 0x800 - (size % 0x800)
-        address += paddingSize
+        fileBytes = f2.read(size)
+        f1.write(fileBytes)
+        address += size
 
-    fileBytes = f2.read(size)
-    f1.write(fileBytes)
-    address += size
+        if needPadCheck == True:
+            f1.write(bytearray([0x00]) * paddingSize)
+            needPadCheck = False
+        f2.close()
+    print("Done!")
 
-    if needPadCheck == True:
-        f1.write(bytearray([0x00]) * paddingSize)
-        needPadCheck = False
-    f2.close()
-print("Done!")
+    f0.close()
+    f1.close()
 
-f0.close()
-f1.close()
+if __name__ == "__main__":
+    main()
--- Unpack.py
+++ Unpack.py
@@ -2,31 +2,34 @@ import os
 import struct
 import argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir")
+    parser.add_argument("wad")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dir")
-parser.add_argument("wad")
-args = parser.parse_args()
+    print("Unpacking WAD...")
+    f0 = open(args.dir, "rb")
+    f1 = open(args.wad, "rb")
+    os.mkdir(args.wad[:-4])
+    numOfFiles = struct.unpack('I', f0.read(4))[0]
 
-print("Unpacking WAD...")
-f0 = open(args.dir, "rb")
-f1 = open(args.wad, "rb")
-os.mkdir(args.wad[:-4])
-numOfFiles = struct.unpack('I', f0.read(4))[0]
+    for i in range(0, numOfFiles):
+        name = f0.read(0x40).decode()
+        editedName = name.replace('\0', '')
+        print(editedName)
+        size = struct.unpack('I', f0.read(4))[0]
+        address = struct.unpack('I', f0.read(4))[0]
+        f1.seek(address, os.SEEK_SET)
+        fileBytes = f1.read(size)
+        filePath = os.path.join(args.wad[:-4], editedName)
+        f2 = open(filePath, "wb")
+        f2.write(fileBytes)
+        f2.close()
+    print("Done!")
 
-for i in range(0, numOfFiles):
-    name = f0.read(0x40).decode()
-    editedName = name.replace('\0', '')
-    print(editedName)
-    size = struct.unpack('I', f0.read(4))[0]
-    address = struct.unpack('I', f0.read(4))[0]
-    f1.seek(address, os.SEEK_SET)
-    fileBytes = f1.read(size)
-    filePath = os.path.join(args.wad[:-4], editedName)
-    f2 = open(filePath, "wb")
-    f2.write(fileBytes)
-    f2.close()
-print("Done!")
+    f0.close()
+    f1.close()
 
-f0.close()
-f1.close()
+if __name__ == "__main__":
+    main()
